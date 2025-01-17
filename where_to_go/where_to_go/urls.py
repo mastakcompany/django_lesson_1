5ac55371bae00d646586ee798256524bfc926dd1
mastak <companymastak@gmail.com>
2025-01-18 00:53:38 +0700

    Create places app

--- where_to_go/where_to_go/urls.py
+++ where_to_go/where_to_go/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from main import views as main_views
+from places import views as places_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', main_views.index, name='home'),
+    path('', places_views.index, name='home'),
 ]
